629c2704ae556997db1940a9e759ea79e59f5b46
racimfethallah <ricdev.io@gmail.com>
2024-07-07 14:52:20 +0100

    updated login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -78,13 +78,12 @@ export default function Login({
 
       <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
         <label htmlFor="username">
-          Username
+          Username (not required for sign in)
         </label>
         <input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           name="username"
           placeholder="username"
-          required
         />
         <label className="text-md" htmlFor="email">
           Email
